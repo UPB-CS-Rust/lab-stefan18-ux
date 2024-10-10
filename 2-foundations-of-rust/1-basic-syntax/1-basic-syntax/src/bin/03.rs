be45aa83435e0e1bbdefa659fad65d44c3753737
Stefan Antoniu Oprea <ellioth@Stefans-MacBook-Pro.local>
2024-10-10 17:01:38 +0300

    First commit

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,16 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
-
+    let mut max = 0;
+    let mut min = 100;
+    for i in input {
+        if i > max {
+            max = i;
+        }
+        if i < min {
+             min = i;
+        }
+    }
     // TODO
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", max, min);
 }
